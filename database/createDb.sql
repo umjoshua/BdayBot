88e00e874a5b6d3d487d5707d295961607629614
Joshua Mathew <umjoshua@gmail.com>
2023-01-09 19:13:01 +0530

    database table

--- database/createDb.sql
+++ database/createDb.sql
@@ -4,9 +4,8 @@ CREATE TABLE users (
 
 CREATE TABLE birthday_reminder(
     id INT PRIMARY KEY,
-    date DATE NOT NULL,
+    date TEXT CHECK(date_column GLOB '[0-1][0-9]-[0-3][0-9]'),
     name TEXT NOT NULL,
     flag INT NOT NULL,
     userId INT REFERENCES users(userId)
 );
-
