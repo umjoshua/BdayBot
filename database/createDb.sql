287f93e858c5174569dbc2fc788e7bd754f32a66
Joshua Mathew <umjoshua@gmail.com>
2023-01-10 18:47:54 +0530

    sql fixed

--- database/createDb.sql
+++ database/createDb.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
 
 CREATE TABLE birthday_reminder(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    date TEXT CHECK(date GLOB '[0-1][0-9]-[0-3][0-9]'),
+    date TEXT CHECK(date GLOB '[0-3][0-9]-[0-1][0-9]'),
     name TEXT NOT NULL,
     userId INT REFERENCES users(userId)
 );
